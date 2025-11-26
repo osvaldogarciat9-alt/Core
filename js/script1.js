91bf403b92bd57991c819ed1ac2489295e391bd7
Osxss <osvaldogarciat9@gmail.com>
2025-11-25 18:46:47 -0600

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,6 +1,5 @@
-
-  if(navigator.serviceWorker){
-  navigator.serviceWorker.register('./sw.js');
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/Core/sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err));
 }
